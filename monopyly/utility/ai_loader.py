7539ce029b6224f9299214f6e552dcbe4a76acbb
Brett Hutley <brett@hutley.net>
2026-07-30 08:01:03 +1000

    Support Python 3.14. Filter to directories under 'AI/'

--- monopyly/utility/ai_loader.py
+++ monopyly/utility/ai_loader.py
@@ -1,6 +1,40 @@
 import os
-import imp
+import sys
+import importlib.util
 import inspect
+
+
+def _is_ai_package(package_folder):
+    '''
+    Returns True if the folder passed in is a Python package, ie a
+    folder holding an __init__.py.
+    '''
+    if os.path.basename(package_folder) == "__pycache__":
+        return False
+
+    return os.path.isfile(os.path.join(package_folder, "__init__.py"))
+
+
+def _load_package(package_name, package_folder):
+    '''
+    Loads the package in the folder passed in and returns the module object.
+
+    This is the replacement for the removed imp.load_package(). The package is
+    registered in sys.modules before it is executed, so that relative imports
+    inside it (eg "from .sophie import SophieAI") resolve.
+    '''
+    init_file = os.path.join(package_folder, "__init__.py")
+    spec = importlib.util.spec_from_file_location(
+        package_name,
+        init_file,
+        submodule_search_locations=[package_folder])
+    if spec is None or spec.loader is None:
+        raise ImportError("Could not load the AI package: {0}".format(package_folder))
+
+    package = importlib.util.module_from_spec(spec)
+    sys.modules[package_name] = package
+    spec.loader.exec_module(package)
+    return package
 
 
 def _find_derived_classes(package, base_class):
@@ -39,15 +73,17 @@ def load_ais():
     '''
     from ..game import PlayerAIBase
 
-    # We find the AI package folders, which live in the "AIs" folder...
-    ai_folders = [item for item in os.listdir("AIs")]
+    # We find the AI package folders, which live in the "AIs" folder.
+    # We ignore anything which is not a package, ie loose files such as
+    # .DS_Store as well as __pycache__ folders...
+    ai_folders = [item for item in os.listdir("AIs") if _is_ai_package("AIs/" + item)]
 
     # We loop through the packages...
     ais = []
     for ai_folder in ai_folders:
         # We load each package...
         package_folder = "AIs/" + ai_folder
-        ai_package = imp.load_package(ai_folder, package_folder)
+        ai_package = _load_package(ai_folder, package_folder)
 
         # We find the classes they expose that are derived from the
         # base AI class...
